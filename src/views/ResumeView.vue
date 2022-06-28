9eed9a1748039ccf3edc0a6cc8c32f8e792f34c3
Kevin Redmond <kred1150@gmail.com>
2022-06-28 18:59:55 -0500

    Added a link to App.vue to take you to the resumes page, added route links to index.js file and created a ResumeView file

---
+++ src/views/ResumeView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Community Page!!!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
