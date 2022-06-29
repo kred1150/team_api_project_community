83e4e7b25dc47b3ea1def3276111282daf972924
Kevin Redmond <kred1150@gmail.com>
2022-06-28 21:30:52 -0500

    added a basic profile page to the show paw for each individual student, still hard-oded. Working on integrating twitter feed now

--- src/views/StudentsShow.vue
+++ src/views/StudentsShow.vue
@@ -13,12 +13,15 @@ export default {
       phone_number: "479-305-9290",
       short_bio: "I was in Big Daddy",
       LinkedIn_URL: "https://www.linkedin.com/in/adam-heckathorn-202072210/",
-      twitter_handle: "@AdamSandler",
+      twitter_handle: "AdamSandler",
       personal_blog: "",
       online_resume_url: "",
       github_url: "",
       photo: "https://st.depositphotos.com/1694341/1303/i/950/depositphotos_13037321-stock-photo-adam-sandler.jpg",
     };
+  },
+  $nextTick: function () {
+    this.twitter_handle.widgets.load();
   },
   methods: {},
 };
@@ -26,7 +29,38 @@ export default {
 
 <template>
   <div class="home">
-    <h1>{{ student }}</h1>
+    <h1>{{ student.first_name }} {{ student.last_name }}</h1>
+    <div class="card" style="width: 18rem">
+      <img v-bind:src="student.photo" class="card-img-top" alt="" />
+      <div class="card-body">
+        <p>linkedin and github links will go here</p>
+      </div>
+    </div>
+    <p>
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapseWidthExample"
+        aria-expanded="false"
+        aria-controls="collapseWidthExample"
+      >
+        Toggle Contact Information
+      </button>
+    </p>
+    <div style="min-height: 120px">
+      <div class="collapse collapse-horizontal" id="collapseWidthExample">
+        <div class="card card-body" style="width: 300px">
+          Email: {{ student.email }} Phone Number: {{ student.phone_number }}
+        </div>
+      </div>
+    </div>
+    <!-- <a class="twitter-timeline" href="https://twitter.com/AdamSandler?ref_src=twsrc%5Etfw">Tweets by AdamSandler</a> -->
+    <div class="card-body">
+      <a data-height="500px" href="https://twitter.com/AdamSandler?ref_src=twsrc%5Etfw" class="twitter-timeline">
+        Adam Sandlers Twitter
+      </a>
+    </div>
   </div>
 </template>
 
@@ -35,3 +69,5 @@ export default {
 <!-- 
   Student.create(first_name:"Adam" last_name:"Heckathorn" email:"ajheckathorn01@gmail.com" phone_number:"479-305-9290" short_bio:"text" LinkedIn_URL:"https://www.linkedin.com/in/adam-heckathorn-202072210/" twitter_handle: personal_blog: online_resume_url: github_url: photo:) 
   -->
+
+<!-- <a class="twitter-timeline" href="https://twitter.com/AdamSandler?ref_src=twsrc%5Etfw">Tweets by AdamSandler</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
