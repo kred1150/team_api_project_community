77a69457e83301f043bfcc3c2a7d1e909ae6f19f
Kevin Redmond <kred1150@gmail.com>
2022-06-29 20:42:19 -0500

    Tested adding a dropdown menu to the navbar with log in/out links as well as create/edit profile links

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon">Resumes-R-Us</span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -20,6 +20,33 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/students">All Students</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Create/Edit Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Sign Up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Log In</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDarkDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Your Profile
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Sign Up</a></li>
+              <li><a class="dropdown-item" href="#">Log In</a></li>
+              <li><a class="dropdown-item" href="#">Create/Edit Profile</a></li>
+            </ul>
           </li>
         </ul>
         <form class="d-flex">
